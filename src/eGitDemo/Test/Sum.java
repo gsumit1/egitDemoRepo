c0966f7339aacd8e006bb676cf211eb5cf7e4f9d
Sumit Ghosh <ghosh.sumit1@gmail.com>
2022-03-29 10:18:39 +0530

    update the code

--- src/eGitDemo/Test/Sum.java
+++ src/eGitDemo/Test/Sum.java
@@ -3,9 +3,14 @@ package eGitDemo.Test;
 public class Sum {
 
 	public static void main(String[] args) {
+		System.out.println("sum of numbers");
+		System.out.println("1st variable");
 		int a = 10;
+		System.out.println("2nd variable");
 		int b = 20;
+		System.out.println("3rd variable");
 		int c=30;
+		System.out.println("Sum of 3 numbers");
 		int sum = a + b+c;
 		System.out.println(sum);
 
